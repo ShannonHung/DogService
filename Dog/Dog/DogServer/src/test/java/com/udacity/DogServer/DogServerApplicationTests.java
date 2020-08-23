a3b70948e227d9d6a4635b1f5c47ea0c02351494
ShannonHung <b10709036@mail.ntust.edu.tw>
2020-08-23 14:07:27 +0800

    dog all

---
+++ Dog/Dog/DogServer/src/test/java/com/udacity/DogServer/DogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.DogServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
