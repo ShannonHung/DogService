db61dc5c364a0db93d070ef27d2d54a9ba84c71a
ShannonHung <b10709036@mail.ntust.edu.tw>
2020-08-23 13:58:48 +0800

    microservice register eureka

---
+++ Dog/dogclient/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'SHANNON', 'APPLE', 'taiwan');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'gwan', 'banana', 'usa');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'mom', 'rice', 'taiwan');
+INSERT INTO item (id, name, description) VALUES (1, 'book','A popular book');
+INSERT INTO item (id, name, description) VALUES (2, 'candy','A favorite candy');
+INSERT INTO item (id, name, description) VALUES (3, 'toy','A simple toy');
+INSERT INTO item (id, name, description) VALUES (4, 'food','Your favorite food item');
+INSERT INTO item (id, name, description) VALUES (5, 'tv','A flat screen TV');
